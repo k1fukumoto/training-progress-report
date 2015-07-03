d50e0fa97ce5083e5e8a4c1ee3b3495dba352b75
Kaoru Fukumoto <k1fukumoto@gmail.com>
2015-07-03 17:29:22 +0900

    first iteration done

--- src/main.py
+++ src/main.py
@@ -1,3 +1,19 @@
-#!/usr/bin/python
+from importer import import_cert_and_contact
+import web
+import json
 
-print "Hello World\n";
+urls = (
+    '/report', 'Report'
+)
+
+class Report:
+    def GET(self):
+        return "OK"
+
+    def POST(self):
+        rep = import_cert_and_contact(web.data())
+        return json.dumps(rep,indent=4) + "\n"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
