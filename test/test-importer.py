18d4705289bd03a8bc176e9af651dff9aa65f6db
Kaoru Fukumoto <k1fukumoto@gmail.com>
2015-07-29 20:24:26 +0900

    Add test for cert-by-month

--- test/test-importer.py
+++ test/test-importer.py
@@ -1,13 +1,20 @@
 import unittest
 import sys
 from collections import defaultdict
-from importer import build_cid_map, load_contact_cert
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from importer import build_cid_map, load_contact_cert, build_cert_by_month
 
-CERTLIST = ['VSP','VTSP','VCP6-DCV','VCA-DCV']
+CFGS = {
+    'cert_acronyms': ['VSP','VTSP','VCP6-DCV','VCA-DCV','VSP - CP','VOP-CP'],
+    'date_range': {'start': datetime(2013,7,1),
+                   'end': datetime(2015,8,1),
+                   'step': relativedelta(months=+1)}
+}
 
 class TestBuildCIDMap(unittest.TestCase):
     def setUp(self):
-        self.certlist = CERTLIST
+        self.certlist = CFGS['cert_acronyms']
         self.data = build_cid_map(self.certlist)
 
     def test_loaded_data(self):
@@ -20,29 +27,42 @@ class TestBuildCIDMap(unittest.TestCase):
             for id in ids:
                 self.assertEqual(self.data['reverse'][id],cert)
 
-
-class TestLoadContactCert(unittest.TestCase):
+class TestBuildCertByMonth(unittest.TestCase):
     def setUp(self):
-        self.certlist = CERTLIST
-        scsv = ''
+        self.certlist = CFGS['cert_acronyms']
+        self.scsv = ''
         with open('sample/cert_and_contact.csv', 'r') as f:
-            scsv = f.read()
-        self.data = load_contact_cert(scsv,self.certlist)
+            self.scsv = f.read()
 
-    def test_loaded_data(self):
-        counts = {'ad':defaultdict(int), 'ed':defaultdict(int)}
+    def test_cert_by_month(self):
+        cc = load_contact_cert(self.scsv,self.certlist)
 
-        for email, certdict in self.data.items():
+        counts_a = {'ad':defaultdict(int), 'ed':defaultdict(int)}
+        for email, certdict in cc.items():
             for cert in certdict:
                 self.assertTrue(cert in self.certlist)
                 for dtype in ['ad','ed']:
                     self.assertTrue(dtype in certdict[cert])
                     if len(certdict[cert][dtype])>0:
-                        counts[dtype][cert] += 1
+                        counts_a[dtype][cert] += 1
 
         for cert in self.certlist:
-            self.assertTrue(cert in counts['ad'])
-            print "## {}: {}".format(cert,counts['ad'][cert])
+            self.assertTrue(cert in counts_a[dtype])
+            self.assertGreaterEqual(counts_a['ed'][cert],counts_a['ad'][cert])
+
+        counts_b = {'ad':defaultdict(int), 'ed':defaultdict(int)}
+        for cdate, cdict in build_cert_by_month(self.scsv,CFGS).items():
+            for cert, cntdict in cdict.items():
+                for dtype in ['ad','ed']:
+                    counts_b[dtype][cert] += cntdict[dtype]
+
+        for dtype in ['ad','ed']:
+            for cert in counts_a[dtype]:
+                try:
+                    self.assertEqual(counts_a[dtype][cert],counts_b[dtype][cert])
+                except:
+                    print "{}:{} - {} != {}".format(dtype,cert,counts_a[dtype][cert],counts_b[dtype][cert])
+                    pass
 
 if __name__ == '__main__':
     unittest.main()
